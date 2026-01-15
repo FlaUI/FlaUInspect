aac6b39ed505ede65fab3f94b448d76927151540
Mitch Capper <mitch.capper@gmail.com>
2026-01-15 08:03:36 -0800

    Made sure all our pattern calls are now try/catch safe to avoid crashing

--- src/FlaUInspect/ViewModels/ElementViewModel.cs
+++ src/FlaUInspect/ViewModels/ElementViewModel.cs
@@ -11,6 +11,7 @@ using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using MenuItem = System.Windows.Controls.MenuItem;
+using User32=FlaUI.Core.WindowsAPI.User32;
 namespace FlaUInspect.ViewModels;
 
 public class ElementViewModel(AutomationElement? automationElement, ILogger? logger) : ObservableObject {
@@ -18,6 +19,7 @@ public class ElementViewModel(AutomationElement? automationElement, ILogger? log
     public AutomationElement? AutomationElement { get; } = automationElement;
     private RelayCommand? _refreshItemCommand;
     private RelayCommand? _focusCommand;
+    private RelayCommand? _focusNativeCommand;
 
     public bool IsExpanded {
         get => GetProperty<bool>();
@@ -51,24 +53,55 @@ public class ElementViewModel(AutomationElement? automationElement, ILogger? log
         });
 
     public ICommand FocusCommand =>
-        _focusCommand ??= new((_) => {
-            try {
-                AutomationElement.Focus();
-            } catch { }
-        });
+        _focusCommand ??= CreateDelayedSafeCommand(AutomationElement.Focus);
 
+    public ICommand FocusNativeCommand =>
+        _focusNativeCommand ??= CreateDelayedSafeCommand(DoNativeFocus);
+
+    private async void DoNativeFocus() {
+        try{
+        //await Task.Delay(250);
+            if (AutomationElement.Properties.ProcessId.TryGetValue(out int processId)){
+                var proc = System.Diagnostics.Process.GetProcessById(processId);
+                var res = User32.SetForegroundWindow(proc.MainWindowHandle);
+                await Task.Delay(100);
+            }
+            AutomationElement.FocusNative();
+        }catch(Exception ex){
+            System.Diagnostics.Debug.WriteLine(ex.ToString());
+        }
+    }
+
+    private static RelayCommand CreateDelayedSafeCommand(Action action, int delayMs=250){
+        var delayedAction = CreateDelayedSafeAction(action,delayMs);
+        return new RelayCommand(_ => delayedAction());
+    }
+    /// <summary>
+    /// Mouse/focus related commands need a delay to allow our mosue action t obe handled first or we resteal the mouse
+    /// </summary>
+    /// <param name="action"></param>
+    /// <param name="delayMs"></param>
+    /// <returns></returns>
+    public static Action CreateDelayedSafeAction(Action action, int delayMs=250) {
+        return async () => {
+            try {
+                await Task.Delay(delayMs);
+                action();
+            } catch { }
+        };
+    }
 
     private ObservableCollection<MenuItem>? _mouseActions;
     public ObservableCollection<MenuItem> MouseActions { get => _mouseActions ??= BuildMouseActions(); }
 
     private ObservableCollection<MenuItem> BuildMouseActions() {
         return [
-            CreateMenuItem("Left Click", () => AutomationElement?.Click()),
-            CreateMenuItem("Right Click", () => AutomationElement?.RightClick()),
-            CreateMenuItem("Double Click", () => AutomationElement?.DoubleClick()),
+            CreateMenuItem("Left Click", CreateDelayedSafeAction(() => AutomationElement?.Click())),
+            CreateMenuItem("Right Click", CreateDelayedSafeAction(() => AutomationElement?.RightClick())),
+            CreateMenuItem("Double Click", CreateDelayedSafeAction(() => AutomationElement?.DoubleClick())),
         ];
     }
-
+    private MenuItem CreateMenuItem(string header, Action<object> value) => CreateMenuItem(header,()=>value(default));
     private MenuItem CreateMenuItem(string header, Action value) {
         return new MenuItem {
             Header = header,
